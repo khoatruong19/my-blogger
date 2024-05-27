3d4ead248eaafdcf771107e78a4ac1b4639e3229
khoatruong19 <khoa-truong@cybozu.vn>
2024-05-27 17:06:52 +0700

    feat: bookmark entry

---
+++ src/app/bookmark/page.tsx
@@ -0,0 +1,5 @@
+import SectionLayout from '@/components/page-layout';
+
+export default function BookmarkPage() {
+  return <SectionLayout pageTitle='Bookmark'>bookmark</SectionLayout>;
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 import { NavItem } from './types';
 import { AiOutlineHome, AiFillHome } from 'react-icons/ai';
-import { CiSearch } from 'react-icons/ci';
+import { CiSearch, CiBookmark } from 'react-icons/ci';
 import { RiSearchFill } from 'react-icons/ri';
+import { IoBookmark } from 'react-icons/io5';
 
 export const NAV_ITEMS: NavItem[] = [
   {
@@ -15,5 +16,11 @@ export const NAV_ITEMS: NavItem[] = [
     icon: CiSearch,
     activeIcon: RiSearchFill,
     path: '/search',
+  },
+  {
+    label: 'Bookmark',
+    icon: CiBookmark,
+    activeIcon: IoBookmark,
+    path: '/bookmark',
   },
 ];
