54f9ef3fd86c8629a1b1f12ebeb483ad41f253dc
khoatruong19 <khoa-truong@cybozu.vn>
2024-06-03 13:20:21 +0700

    fix: move dashboard nav item to sidebar

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,8 @@ import { AiOutlineHome, AiFillHome } from 'react-icons/ai';
 import { CiSearch, CiBookmark } from 'react-icons/ci';
 import { RiSearchFill } from 'react-icons/ri';
 import { IoBookmark } from 'react-icons/io5';
+import { LuLayoutDashboard } from 'react-icons/lu';
+import { RiDashboardFill } from 'react-icons/ri';
 
 export const NAV_ITEMS: NavItem[] = [
   {
@@ -24,3 +26,10 @@ export const NAV_ITEMS: NavItem[] = [
     path: '/bookmark',
   },
 ];
+
+export const DASHBOARD_NAV_ITEM = {
+  label: 'Dashboard',
+  icon: LuLayoutDashboard,
+  activeIcon: RiDashboardFill,
+  path: '/dashboard',
+};
